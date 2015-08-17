02132d74a64bd147190e5b86a8d851519c41858a
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-17 15:30:31 -0600

    with new page created, user can add new books to database

--- books/routes/books.js
+++ books/routes/books.js
@@ -4,8 +4,22 @@ var db = require('monk')('localhost/myBooks');
 var booksCollection = db.get('books');
 
 router.get('/books/index', function (req, res, next) {
-  res.render('books/index')
+  booksCollection.find({}, function (err, books) {
+    res.render('books/index', {books: books})
+  });
 });
+
+router.get('/books/new', function (req, res, next) {
+  res.render('books/new')
+});
+
+router.post('/books/create', function (req, res, next) {
+  booksCollection.insert(req.body).then(function (book) {
+    res.redirect('/books/show')
+  });
+});
+
+
 
 
 module.exports = router;
