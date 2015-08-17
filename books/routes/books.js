527d0da6f427a7aaea76cf5910087ef2d56d4e6d
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-17 16:15:31 -0600

    users can now update and delete previous entries

--- books/routes/books.js
+++ books/routes/books.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db = require('monk')('localhost/myBooks');
 var booksCollection = db.get('books');
 
-router.get('/books/index', function (req, res, next) {
+router.get('/books', function (req, res, next) {
   booksCollection.find({}, function (err, books) {
     res.render('books/index', {books: books})
   });
@@ -25,6 +25,23 @@ router.get('/books/show', function (req, res, next) {
   });
 });
 
+router.get('/books/:id/edit', function (req, res, next) {
+  booksCollection.findOne({_id: req.params.id}, function (err, book) {
+    res.render('books/edit', {book: book})
+  });
+});
+
+router.post('/books/:id/update', function (req, res, next) {
+  booksCollection.update({_id: req.params.id}, req.body).then(function (book) {
+    res.redirect('/books')
+  });
+});
+
+router.post('/books/:id/delete', function (req, res, next) {
+  booksCollection.remove({_id: req.params.id}, req.body).then(function (book) {
+    res.redirect('/books')
+  });
+});
 
 
 
