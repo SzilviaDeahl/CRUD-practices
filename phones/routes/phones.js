b1f8692de348c079d1d02eb59ec26196f90b5bfb
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-17 11:16:28 -0600

    succesfully creating new items and adding them to users database

--- phones/routes/phones.js
+++ phones/routes/phones.js
@@ -1,7 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/myPhones');
+var phonesCollection = db.get('phones');
+
+router.get('/phones', function (req, res, next) {
+  phonesCollection.find({}, function (err, phones) {
+    console.log(phones);
+    res.render('phones/index', {phones: phones})
+  });
+});
 
 router.get('/phones/new', function (req, res, next) {
   res.render('phones/new')
 });
+
+router.post('/phones/create', function (req, res, next) {
+  phonesCollection.insert(req.body).then(function (phone) {
+    console.log(req.body);
+    res.redirect('/phones/show')
+  });
+});
+
+router.get('/phones/show', function (req, res, next) {
+  res.render('phones/show')
+});
+
 module.exports = router;
