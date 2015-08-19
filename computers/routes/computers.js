80ba26aac50d25fd5904d95fbcf4fd029274d9ca
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-19 15:19:08 -0600

    users can edit/delete previous entries and view all entries in show page

--- computers/routes/computers.js
+++ computers/routes/computers.js
@@ -15,19 +15,27 @@ router.get('/computers/new', function (req, res, next) {
 
 router.post('/computers/create', function (req, res, next) {
   compCollections.insert(req.body).then(function (computer) {
+    res.redirect('/computers/show', {computers: computers})
+  });
+});
+
+router.get('/computers/:id/edit', function (req, res, next) {
+  compCollections.findOne({_id: req.params.id}, function (err, computer) {
+    res.render('computers/edit', {computer: computer})
+  });
+});
+
+router.post('/computers/:id/update', function (req, res, next) {
+  compCollections.update({_id: req.params.id}, req.body).then(function (computer) {
     res.redirect('/computers/index')
   });
 });
 
-
-
-
-
-
-
-
-
-
+router.post('/computers/:id/delete', function (req, res, next) {
+  compCollections.remove({_id: req.params.id}, req.body).then(function (computer) {
+    res.redirect('/computers/index')
+  });
+});
 
 
 module.exports = router;
