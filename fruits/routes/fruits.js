56cf88f1834f7c39246c40bc27b06c6ae13dbb52
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-19 16:11:12 -0600

    user can create new data entries

--- fruits/routes/fruits.js
+++ fruits/routes/fruits.js
@@ -9,10 +9,21 @@ router.get('/fruits/index', function (req, res, next) {
   });
 });
 
+router.get('/fruits/new', function (req, res, next) {
+  res.render('fruits/new')
+});
 
+router.post('/fruits/create', function (req, res, next) {
+  fruitCollection.insert(req.body).then(function (fruit) {
+    res.redirect('/fruits/show')
+  });
+});
 
-
-
+router.get('/fruits/show', function (req, res, next) {
+  fruitCollection.find({}, function (err, fruits) {
+    res.render('fruits/show', {fruits: fruits})
+  });
+});
 
 
 
