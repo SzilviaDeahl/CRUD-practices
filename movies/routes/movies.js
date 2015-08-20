e30c1b4cdb41a4f0a0d870616072646248e71fd9
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-20 11:06:49 -0600

    user can create new entries to database

--- movies/routes/movies.js
+++ movies/routes/movies.js
@@ -9,4 +9,20 @@ router.get('/movies/index', function (req, res, next) {
   });
 });
 
+router.get('/movies/new', function (req, res, next) {
+  res.render('movies/new')
+});
+
+router.post('/movies/create', function (req, res, next) {
+  movieCollection.insert(req.body).then(function (movie) {
+    res.redirect('/movies/show')
+  });
+});
+
+router.get('/movies/show', function (req,res, next) {
+  movieCollection.find({}, function (err, movies) {
+    res.render('movies/show', {movies:movies})
+  });
+});
+
 module.exports = router;
