c83a0c8775ec52c121a10af51267d3aeb68287ff
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-20 10:39:13 -0600

    user can add and edit information

--- personal/routes/personal.js
+++ personal/routes/personal.js
@@ -9,5 +9,32 @@ router.get('/personal/index', function (req, res, next) {
   });
 });
 
+router.get('/personal/new', function (req, res, next) {
+  res.render('personal/new')
+});
+
+router.post('/personal/create', function (req, res, next) {
+  infoCollection.insert(req.body).then(function (inf) {
+    res.redirect('/personal/show', {inf: inf})
+  });
+});
+
+router.get('/personal/show', function (req, res, next) {
+  infoCollection.find({}, function (err, info) {
+    res.render('personal/show', {info: info})
+  });
+});
+
+router.get('/personal/:id/edit', function (req, res, next) {
+  infoCollection.findOne({_id: req.params.id}, function (err, inf) {
+    res.render('personal/edit', {inf:inf})
+  });
+});
+
+router.post('/personal/:id/update', function (req, res, next) {
+  infoCollection.update({_id: req.params.id}, req.body).then(function (inf) {
+    res.redirect('/personal/index')
+  });
+});
 
 module.exports = router;
