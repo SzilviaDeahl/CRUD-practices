8706ddf16a7352d5399516e63b4efce998f77044
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-31 11:39:18 -0600

    index page redirects to people index page

--- people/routes/people.js
+++ people/routes/people.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/myPeople');
+var peopleCollection = db.get('people');
 
-
+router.get('/people/index', function (req, res, next) {
+  peopleCollection.find({}, function (err, people) {
+    res.render('people/index', {people: people})
+  })
+})
 
 module.exports = router;
