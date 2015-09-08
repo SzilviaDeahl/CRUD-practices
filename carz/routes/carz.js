8bba3f845ad6404827d1bb5c8f5bd55fd21d2f67
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-09-08 16:49:23 -0600

    route to new page added and new page created

--- carz/routes/carz.js
+++ carz/routes/carz.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/myCarz');
+var carzCollections = db.get('carz');
+
 
 router.get('/carz/index', function (req, res, next) {
-  res.render('carz/index')
+  carzCollections.find({}, function (err, carz) {
+    res.render('carz/index', {carz: carz})
+  });
+});
+
+router.get('/carz/new', function (req, res, next) {
+  res.render('carz/new')
 });
 
 module.exports = router;
