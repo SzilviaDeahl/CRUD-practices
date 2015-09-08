4d99f4b73253e2d4e250b015dc96ae3c2804b99c
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-09-08 17:22:00 -0600

    user can now add new data and view them

--- carz/routes/carz.js
+++ carz/routes/carz.js
@@ -14,4 +14,17 @@ router.get('/carz/new', function (req, res, next) {
   res.render('carz/new')
 });
 
+router.post('/carz/create', function (req, res, next) {
+  carzCollections.insert(req.body).then(function (car) {
+    res.redirect('/carz/show')
+  });
+});
+
+router.get('/carz/show', function (req, res, next) {
+  carzCollections.find({}, function (err, carz) {
+    res.render('carz/show', {carz: carz})
+  });
+});
+
+
 module.exports = router;
